a5c7820964c4e042f0906415db403c5031be696e
manohar774 <manohar774@github.com>
2024-09-13 12:32:00 +0000

    second commit

---
+++ second.sh
@@ -0,0 +1,25 @@
+
+
+
+# This is my second code 
+
+
+
+#Addition
+ x=a+b
+ a=5 
+ b=7
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+ #Subtraction 
+=======
+ #
+ #
+ #
+ #3a0e4ba (this is my fourth commit)
+=======
+ # division
+ #
+>>>>>>> 3a0e4ba (this is my fourth commit)
